04a9ae2b675fc1df696872023aca9b223ae6fca8
DDSRem <73049927+DDSRem@users.noreply.github.com>
2025-01-12 19:23:25 +0800

    Update strmhelper.py

--- plugins.v2/cloudterminator/strmhelper.py
+++ plugins.v2/cloudterminator/strmhelper.py
@@ -11,9 +11,15 @@ class StrmHelper:
     解析数据库，生成 STRM 文件
     """
 
-    def __init__(self, db_name: str):
+    def __init__(self, db_name: str, client):
         self.connection = sqlite3.connect(db_name)
+        self.client = client
         self.path_list = []
+        self.rmt_mediaext = ['.mp4', '.mkv', '.ts', '.iso',
+                        '.rmvb', '.avi', '.mov', '.mpeg',
+                        '.mpg', '.wmv', '.3gp', '.asf',
+                        '.m4v', '.flv', '.m2ts', '.tp',
+                        '.f4v']
 
     def get_video_file_path(self, parent_id: int):
         """
@@ -28,15 +34,10 @@ class StrmHelper:
                 self.path_list.append([path, file_parent_id])
         return self.path_list
 
-    def generate_strm_files(self, parent_id, target_dir, server_address, database="strm_db.sqlite"):
+    def generate_strm_files_db(self, parent_id, target_dir, server_address, database="strm_db.sqlite"):
         """
         生成 STRM 文件并存储信息到 SQLite 数据库
         """
-        rmt_mediaext = ['.mp4', '.mkv', '.ts', '.iso',
-                        '.rmvb', '.avi', '.mov', '.mpeg',
-                        '.mpg', '.wmv', '.3gp', '.asf',
-                        '.m4v', '.flv', '.m2ts', '.tp',
-                        '.f4v']
 
         if parent_id != 0:
             removal_path = get_path(self.connection, parent_id)
@@ -61,7 +62,7 @@ class StrmHelper:
             file_name = os.path.splitext(original_file_name)[0] + ".strm"
             new_file_path = os.path.join(file_target_dir, file_name)
 
-            if os.path.splitext(original_file_name)[1] not in rmt_mediaext:
+            if os.path.splitext(original_file_name)[1] not in self.rmt_mediaext:
                 logger.warn("跳过网盘路径： %s", file_path.replace(target_dir, '', 1))
                 continue
 
@@ -79,5 +80,54 @@ class StrmHelper:
 
             cursor.execute('INSERT INTO strm_files VALUES (?,?)', (new_file_path, content))
             logger.info("生成 %s", new_file_path)
+        conn.commit()
+        conn.close()
+
+    def generate_strm_files(self, parent_id: int, target_dir, server_address, database="strm_db.sqlite"):
+        """
+        生成 STRM 文件并存储信息到 SQLite 数据库
+        """
+
+        if parent_id != 0:
+            removal_path = get_path(self.connection, parent_id)
+        else:
+            removal_path = ''
+
+        target_dir = target_dir.rstrip("/")
+        server_address = server_address.rstrip("/")
+
+        conn = sqlite3.connect(database)
+        cursor = conn.cursor()
+
+        cursor.execute('''CREATE TABLE IF NOT EXISTS strm_files
+                        (file_path TEXT, content TEXT)''')
+        conn.commit()
+
+        attr = self.client.fs.attr(parent_id)
+        file_path = attr['path']
+        file_path = os.path.join(f"{target_dir}{file_path.replace(removal_path, '', 1)}")
+        file_target_dir = os.path.dirname(file_path)
+        original_file_name = os.path.basename(file_path)
+        file_name = os.path.splitext(original_file_name)[0] + ".strm"
+        new_file_path = os.path.join(file_target_dir, file_name)
+
+        if os.path.splitext(original_file_name)[1] not in self.rmt_mediaext:
+            logger.warn("跳过网盘路径： %s", file_path.replace(target_dir, '', 1))
+            return
+
+        cursor.execute("SELECT 1 FROM strm_files WHERE file_path=?", (new_file_path,))
+        if cursor.fetchone():
+            logger.warn("跳过 %s", new_file_path)
+            return
+
+        pickcode = attr['pick_code']
+        os.makedirs(os.path.dirname(new_file_path), exist_ok=True)
+
+        content = f"{server_address}/{pickcode}/{original_file_name}"
+        with open(new_file_path, 'w', encoding='utf-8') as file:
+            file.write(content)
+
+        cursor.execute('INSERT INTO strm_files VALUES (?,?)', (new_file_path, content))
+        logger.info("生成 %s", new_file_path)
         conn.commit()
         conn.close()
